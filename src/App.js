377f546b60a67c0057c851d20525376f19ccad4d
WilliamKXiong <mana_grey@hotmail.com>
2021-06-18 04:14:14 -0500

    Basic About Page /about

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 //import './App.css';
-import Home from './Pages/Home/Home';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
 
 function App() {
   return (
     <Router>
           <Switch>
-            <Route path="/">
+            <Route exact path="/">
               <Home />
+            </Route>
+            <Route exact path="/about">
+              <About />
             </Route>
           </Switch>
     </Router>
---
+++ src/Pages/About/About.js
@@ -0,0 +1,12 @@
+import './About.css'
+
+const About = () => {
+    return (
+        <div className="About">
+            <h1>About</h1>
+            <h2>This is my online portfolio created with React.js, hosted with Github Pages, and using Google Domain</h2>
+        </div>
+    )
+}
+
+export default About;
