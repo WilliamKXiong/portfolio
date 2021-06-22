c4dc7cc2ad4911ba191ccd7102ad9bc63df59fc7
WilliamKXiong <40574588+WilliamKXiong@users.noreply.github.com>
2021-06-22 14:07:42 -0500

    About page update

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,4 +1,4 @@
-//import './About.css'
+import './About.css'
 import '../../Helper/CommonlyUsed.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Image, Container, Row, Col } from 'react-bootstrap';
@@ -7,19 +7,26 @@ import ProfessionalPhoto from '../../Images/Professional-Photo.jpg'
 const About = () => {
     return (
         <Container>
-            <Row className="pl-5">
+            <Row className="px-5">
                 <h1>About Me</h1>
             </Row>
-            <Row>
-                <Col sm={8} className="pl-2">
-                    <span>This is sample text</span>
+            <Row className="px-5 row-body">
+                <Col sm={8} className="col-text">
+                    <span>
+                        Hello my name is William Xiong, and I am a Computer Science graduate at <a href="https://www.metrostate.edu/">Metropolitan State University.</a>
+                        Before college I served 4 years in the United States Marine Corps as a Transmissions System Operator(radio operator for short).
+                        fdfd
+
+                    </span>
                 </Col>
-                <Col sm={4} className="d-flex justify-content-center">
-                    <Image src={ProfessionalPhoto} roundedCircle fluid/>
+                <Col sm={4} className="d-flex col-text">
+                    <Image src={ProfessionalPhoto} roundedCircle />
                 </Col>
             </Row>
-            <Row>
+            <Row className="px-5 row-body">
+                <Col sm={12} className="col-text">
 
+                </Col>
             </Row>
         </Container>
     )
