3a4870fbe402c3437ed16a9b1f6d6fde61539263
WilliamKXiong <40574588+WilliamKXiong@users.noreply.github.com>
2021-06-22 19:18:17 -0500

    Contact Page

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,15 +1,24 @@
 import './Contact.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Card } from 'react-bootstrap';
+import ButtonLink from '../../Components/ButtonLink/ButtonLink';
 
 const Contact = () => {
+    console.log(process.env);
     return (
         <Container fluid className="text-center">
-            <Row className="row-c">
-                <Col sm={12}>
-                    This page is under construction
-                </Col>
-            </Row>
+                <Row>
+                    <Card>
+                        <Card.Title>
+                            Contact
+                        </Card.Title>
+                        <Card.Body className="">
+                            <Card.Text>
+                                <ButtonLink link="#/" text="none" linkSVG=""/>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Row>
         </Container>
     )
 }
